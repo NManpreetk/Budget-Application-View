bab08e7e1074ea4642c7e0a8199f640f3e2c5bdb
Manpreet kaur <manpreetmanku26@gmail.com>
2018-11-21 10:52:27 -0600

    Account and Category view added

---
+++ BudgetApplicationView/Models/CreateTransactionsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BudgetApplicationView.Models
+{
+    public class CreateTransactionsViewModel
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public decimal Amount { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsVoided { get; set; }
+        public string EnteredById { get; set; }
+    }
+}
---
+++ BudgetApplicationView/Models/ViewCategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BudgetApplicationView.Models
+{
+    public class ViewCategoryViewModel
+    {
+        public string Name { get; set; }
+    }
+}
